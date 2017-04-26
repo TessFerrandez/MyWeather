8af5428338da38d4d5156103aef0e8f6465632fe
Tess Ferrandez <tess_ferrandez@hotmail.com>
2017-04-26 14:48:15 +0200

    Added constants.cs

---
+++ MyWeather.Core/Services/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeather.Core
+{
+    public class Constants
+    {
+        public static string APPID = "your-app-id";
+    }
+}
